1c2c1c0cb4471297fb39e0e1cb9d785cb73f297d
Rodrigo Bernal <rodrigobernal19@gmail.com>
2024-10-14 23:22:35 -0300

    Final version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,82 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import logo from "./assets/image.png";
+import { ChangeEvent, FormEvent, useCallback, useState } from "react";
 
 export default function Home() {
+  const correctData: {matricula: string, codigo: string} = {
+    matricula: "19340104",
+    codigo: "338788"
+  }
+
+  const [matricula, setMatricula] = useState("");
+  const [codigo, setCodigo] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleMatricula = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setMatricula(event.target.value);
+  }, [])
+
+  const handleCodigo = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setCodigo(event.target.value);
+  }, []);
+
+  const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const responseElement = document.querySelector("#response");
+
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+
+      if (!responseElement) return;
+
+      if ((matricula === correctData.matricula) && (codigo === correctData.codigo)) {
+        responseElement.innerHTML = "<p>Aluno: Rodrigo Fernandes Bernal</p><p>Matrícula: 19340104</p><p>CPF: 418.201.978-40</p><p>Curso: CFM - CERTIFICATE IN FINANCIAL MANAGEMENT</p>";
+      } else { 
+        responseElement.innerHTML = "<p>Os dados informados não conferem com nenhum documento emitido pelo Insper.</p><p>Verifique se os dados foram informados corretamente e tente novamente.</p>";
+      }
+    }, 4000)
+  }, [matricula, codigo, correctData.matricula, correctData.codigo])
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <header className={styles.header}>
+        <Image className={styles.logo} src={logo} alt="Insper logo" width="120" height="42"/ >
+      </header>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
+      <div className={styles.content}>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <h1 className={styles.title}>Verificação de Autenticidade</h1>
+
+          <div className={styles.inputWrapper}>
+            <label className={styles.label}>Matrícula</label>
+            <input className={styles.input} type="text" value={matricula} onInput={handleMatricula}/>
+          </div>
+
+          <div className={styles.inputWrapper}>
+            <label className={styles.label}>Código de confirmação</label>
+            <input className={styles.input} type="text" value={codigo} onInput={handleCodigo}/>
+          </div>
+
+          {!isLoading && (
+            <button type="submit" className={styles.button}>Validar</button>
+          )}
+
+          {isLoading && (
+            <button type="submit" className={styles.loadingButton}>Validar</button>
+          )}
+
+          
+          <div id="response" className={styles.response}></div>
+        </form>
+      </div>
+
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <address className={styles.address}>Rua Quatá, 300 - Vila Olímpia - São Paulo/SP - Brasil | CEP: 04546-042 | Tel.: (11) 4504-2400</address>
       </footer>
     </div>
   );
